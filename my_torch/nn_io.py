6698b89602bce09088a2b651777a7435f58728e9
Yanis897349 <146654215+Yanis897349@users.noreply.github.com>
2025-12-09 17:24:40 +0000

    chore(my_torch): ununsed import

--- my_torch/nn_io.py
+++ my_torch/nn_io.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import json
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Callable, Mapping, MutableMapping
+from typing import Any, Callable, Mapping
 
 import numpy as np
 from numpy.typing import NDArray
